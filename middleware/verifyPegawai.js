a02ec377536440da4c234c899b9727f8a464bb21
wibiemahardhika22 <wibie2100016081@webmail.uad.ac.id>
2024-07-13 12:49:22 +0700

    fix: verifikasi pegawai, hanya bisa melakukan get/edit/delete pada akun sendiri

--- middleware/verifyPegawai.js
+++ middleware/verifyPegawai.js
@@ -1,5 +1,5 @@
 const verifyPegawai = (req, res, next) => {
-    if (req.user.role === 'pegawai' && req.user.id !== req.params.id) {
+    if (req.user.role === 'Pegawai' && req.user.id !== parseInt(req.params.id)) {
         return res.status(403).json({ message: 'Anda hanya dapat mengakses data diri sendiri' });
     }
     next();
