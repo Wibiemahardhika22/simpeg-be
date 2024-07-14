16a91fa7de3e07713cda8144000c7e417976041b
wibiemahardhika22 <wibie2100016081@webmail.uad.ac.id>
2024-07-14 12:36:57 +0700

    fix: menentukan folder berdasarkan id

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,9 +1,21 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 // Konfigurasi penyimpanan
 const storage = multer.diskStorage({
-    destination: './uploads/',
+    destination: (req, file, cb) => {
+        // Ambil IDPegawai dari parameter route
+        const idPegawai = req.params.id;
+        const uploadPath = path.join('./uploads', idPegawai);
+
+        // Buat folder jika belum ada
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+
+        cb(null, uploadPath);
+    },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`);
     }
